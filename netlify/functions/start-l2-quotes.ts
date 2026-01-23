e29b0a49b443d2708d5c4257968447ac5f7ed619
nvm1410 <vominhbinto@gmail.com>
2026-01-23 09:46:11 +0700

    add error catch

--- netlify/functions/start-l2-quotes.ts
+++ netlify/functions/start-l2-quotes.ts
@@ -4,26 +4,30 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(process.env.SUPABASE_PROJECT_URL!, process.env.SUPABASE_API_KEY!);
 
 export default async (req: Request) => {
-  const body = (await req.json()) as L2QuoteProps;
+  try {
+    const body = (await req.json()) as L2QuoteProps;
 
-  const { data } = await supabase
-    .from("l2_quote_runs")
-    .insert({
-      input: body,
-    })
-    .select()
-    .single();
+    const { data } = await supabase
+      .from("l2_quote_runs")
+      .insert({
+        input: body,
+      })
+      .select()
+      .single();
 
-  fetch(`https://deep.seer.pm/.netlify/functions/l2-quotes-background`, {
-    method: "POST",
-    body: JSON.stringify({ runId: data.id }),
-  });
-  return new Response(JSON.stringify({ runId: data.id }), {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "http://localhost:5173",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET",
-    },
-  });
+    fetch(`https://deep-pm.netlify.app/.netlify/functions/l2-quotes-background`, {
+      method: "POST",
+      body: JSON.stringify({ runId: data.id }),
+    });
+    return new Response(JSON.stringify({ runId: data.id }), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost:5173",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET",
+      },
+    });
+  } catch (e) {
+    return Response.json({ error: e.message ?? "Unknown error" }, { status: 500 });
+  }
 };
